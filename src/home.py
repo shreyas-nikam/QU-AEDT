6ab9ca45c49bcda8c5fbd67b37dd1b902e81e372
Uddhav Zambare <uddhav@qusandbox.com>
2024-02-15 16:02:39 -0500

    Update home.py

--- src/home.py
+++ src/home.py
@@ -19,6 +19,13 @@ from courseMaterial import CourseMaterial
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def main():
+    page1 = Calculations()
+    page2 = WhatIf()
+    page3 = ChatBot()
+    page4 = Quiz()
+    page5 = Reference()
+    page6 = CourseMaterial()
+    
     url: str = st.secrets["SUPABASE_URL"]
     key: str = st.secrets["SUPABASE_KEY"]
     if 'supabaseDB' not in st.session_state:
@@ -53,16 +60,13 @@ def main():
    
     if not st.session_state.userInfo:
         st.header("NY 144 law on Automated Employment Decision Tools (AEDT)", divider= "blue")
-        # st.caption("Please refer to the [user guide]() on information to navigate the application.")
         left_co, cent_co,right_co = st.columns([2,3,2])
         with cent_co:
             index = None
             st.markdown("<p style='text-align: center;'>Enter the following information to proceed.</p>", unsafe_allow_html=True)
-        # left_co, cent_co,right_co = st.columns(3)
-        # with cent_co:
             name = st.text_input("Name :", placeholder="Enter full name")
-            email = st.text_input("Email ID :", placeholder="Enter QuantUniversity email ID")
-            
+            email = st.text_input("Email ID :", placeholder="Enter QuantUniversity email ID")            
+            agree = st.checkbox(f"""I read and agree to QuantUniversity's [Privacy Policy](https://www.quantuniversity.com/privacy.html)""")            
             left_co1, cent_co1,right_co1 = cent_co.columns([3,3,3])
             if cent_co1.button('PROCEED'):
                 if not name:
@@ -73,11 +77,12 @@ def main():
                     st.error("Please enter your email.")
                 elif not validate_email(email):
                     st.error("Please enter a valid QuantUniversity email address.")
+                elif not agree:
+                    st.error("Please read and agree the QuantUniversity's [Privacy Policy](https://www.quantuniversity.com/privacy.html).")
                 else:
                     data, count = st.session_state.supabaseDB.table('aedt_users').insert({"name":name, "email":email}).execute()
                     st.session_state.userInfo = data[1][0]
                     st.rerun()
-
     elif page == "Home" :
         st.header("NY 144 law on Automated Employment Decision Tools (AEDT)", divider= "blue")
         st.markdown("""
@@ -90,23 +95,25 @@ def main():
         - Overall, the rules aim to ensure compliance with the law and promote fairness in employment practices involving AEDTs.
         - It is a process conducted by employers or employment agencies to assess the fairness of an automated employment decision tool (AEDT) in selecting candidates for employment or promotion.
         - It ensures that AEDTs don't unfairly discriminate based on sex, race/ethnicity, or intersectional categories by analyzing selection or scoring patterns and comparing them across demographic groups.
+        
+        ### Usage Guide
+
+        - Course Materials: Slides and Video of the 8 modules
+        - Demo: Bias audit calculations and What-if analysis
+        - QuBot: Ask me anything regarding the NY 144 law on Automated Employment Decision Tools (AEDT)
+        - Quiz and Certificate: Test your understanding and obtain a certificate of completion
+        - Reference PDF: Original publication
+
         """, unsafe_allow_html=True)
         st.divider()
         footer_text = """
-        <p style="font-size: 12px; text-align: center">
+        <p style="font-size: 12px; text-align: left">
             Copyright © 2024. All rights reserved by QuantUniversity <br>
             The purpose of this demonstration is solely for educational use and illustration. To access the full legal documentation, please visit <a href="https://www.nyc.gov/site/dca/about/automated-employment-decision-tools.page">this link</a>. Any reproduction of this demonstration requires prior written consent from QuantUniversity.
         </p>
         """
         st.markdown(footer_text, unsafe_allow_html=True)  
         
-    page1 = Calculations()
-    page2 = WhatIf()
-    page3 = ChatBot()
-    page4 = Quiz()
-    page5 = Reference()
-    page6 = CourseMaterial()
-
     if page == "Bias Audit Calculations":        
         page1.main()
 
@@ -124,14 +131,6 @@ def main():
 
     if page == "Reference PDF":
         page5.main()
-
-    # if page == "Slide Deck":
-    #     slides = SlideDeck()
-    #     slides.display_deck()
-    
-    # if page == "Video":
-    #     video  = Video()
-    #     video.display_video()
 
 if __name__ == "__main__":
     main()
